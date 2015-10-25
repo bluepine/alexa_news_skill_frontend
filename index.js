8a43e103de480fa2ce8ddb139a803deab87d4a39
Song Wei <song.wei@turner.com>
2015-10-25 19:24:16 +0000

    state info and API definition added

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// ArticleListOnTopicIntent top headlines on {politics|Topic}
-// ArticleDetailKeywordIntent tell me the {Clinton|Keyword} story
-// ArticleDetailNumberIntent tell me story {StoryNumber}
-// ArticleDetailOrderIntent tell me the {Order} story
-// NextArticleIntent play the next story
-// PreviousArticleIntent play the previous story
-// NextArticleListIntent more headlines
-// PreviousArticleListIntent previous list of headlines
-// ReferenceArticleDetailKeywordIntent tell me more about {Clinton|Keyword}
-// ArticleListIntent top headlines
-// OldArticleListIntent what did i miss
-// OlderArticleListIntent more that i miss
-// OldArticleListOnTopicIntent what did i miss on {politics|Topic}
-// OlderArticleListOnTopicIntent more that i miss on {politics|Topic}
-// NoInterestForListIntent i don't like this list
-// NoInterestForArticleIntent i don't like this article
-// ArticleListOnDateIntent top headlines {Date}
-// ArticleListOnTopicOnDateIntent top headlines on {politics|Topic} {Date}
-// ReplayArticleIntent replay article
-// ReplayArticleListIntent replay headlines
-
-
-
 // Connect to lambda
 var app = require('./news_app.js')
 exports.handler = app.lambda();
--- news_app.js
+++ news_app.js
@@ -1,3 +1,24 @@
+// ArticleListOnTopicIntent top headlines on {politics|Topic}
+// ArticleDetailKeywordIntent tell me the {Clinton|Keyword} story
+// ArticleDetailNumberIntent tell me story {StoryNumber}
+// ArticleDetailOrderIntent tell me the {Order} story
+// NextArticleIntent play the next story
+// PreviousArticleIntent play the previous story
+// NextArticleListIntent more headlines
+// PreviousArticleListIntent previous list of headlines
+// ReferenceArticleDetailKeywordIntent tell me more about {Clinton|Keyword}
+// ArticleListIntent top headlines
+// OldArticleListIntent what did i miss
+// OlderArticleListIntent more that i miss
+// OldArticleListOnTopicIntent what did i miss on {politics|Topic}
+// OlderArticleListOnTopicIntent more that i miss on {politics|Topic}
+// NoInterestForListIntent i don't like this list
+// NoInterestForArticleIntent i don't like this article
+// ArticleListOnDateIntent top headlines {Date}
+// ArticleListOnTopicOnDateIntent top headlines on {politics|Topic} {Date}
+// ReplayArticleIntent replay article
+// ReplayArticleListIntent replay headlines
+
 function log(text) {
     console.log(text)
 }
@@ -20,39 +41,21 @@ var httpGet = function(url) {
 
 var alexa = require('alexa-app')
 var app = new alexa.app()
-app.intent('getNews', {
-        "slots": {},
-        "utterances": [],
-    },
-    function(request, response) {
-        var url = "https://controller-noyda.c9.io/articlelistontopic/news"
-        httpGet(url).then(function(body) {
-            response.say(JSON.stringify(body)).send()
-            response.send()
-        }).catch(function(err) {
-            response.say('http request failed').send()
-        });
-        return false;
-    }
-)
+// app.intent('getNews', {
+//         "slots": {},
+//         "utterances": [],
+//     },
+//     function(request, response) {
+//         var url = "https://controller-noyda.c9.io/articlelistontopic/news"
+//         httpGet(url).then(function(body) {
+//             response.say(JSON.stringify(body)).send()
+//             response.send()
+//         }).catch(function(err) {
+//             response.say('http request failed').send()
+//         });
+//         return false;
+//     }
+// )
 
-app.intent('reset', {
-        "slots": {},
-        "utterances": [],
-    },
-    function(request, response) {
-        log('reset')
-        var url = "https://controller-noyda.c9.io"
-        R(url, function(error, resp, body) {
-            if (!error && resp.statusCode == 200) {
-                response.say('done').send()
-            }
-            else {
-                response.say('http request failed').send()
-            }
-        })
-        return false
-    }
-)
 
 module.exports = app
