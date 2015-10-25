3dda46f12859a74c13ca800cf5d83eb915419ee2
Song Wei <song.wei@turner.com>
2015-10-25 21:55:31 +0000

    lambda local test added

---
+++ test/ArticleDetailNumberIntent.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "session": {
+    "sessionId": "SessionId.1e945785-af13-4661-88bf-c1aca289811e",
+    "application": {
+      "applicationId": "amzn1.echo-sdk-ams.app.db490dd5-2058-4edc-a8c4-5cc7398149a9"
+    },
+    "attributes": null,
+    "user": {
+      "userId": "amzn1.account.AFKSFC7GBHXUD2WH72PGFPTGXLFA",
+      "accessToken": null
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.a8dd9d95-6a5c-4e2a-b52b-d8dce9517a14",
+    "timestamp": 1445806663788,
+    "intent": {
+      "name": "ArticleDetailNumberIntent",
+      "slots": {
+        "StoryNumber": {
+          "name": "StoryNumber",
+          "value": "6"
+        }
+      }
+    },
+    "reason": null
+  }
+}
